12f157100e28070c9997e176c19afa00a5c1786e
Dennis Mubamba <mubambadennis@gmail.com>
2023-08-14 18:20:52 +0200

    Added new content type

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", `https://loremflickr.com/`, 'https://dl.airtable.com','https://market-assets.strapi.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
